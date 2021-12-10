68eaa8c0323dcb85e34596d4578f0a8574b6b4a5
Fab <fabianjarrett@gmail.com>
2021-12-10 15:44:37 +0000

    Add solution for 2021 day 4 part 1

---
+++ 2021/day4/part1.py
@@ -0,0 +1,40 @@
+def get_rows_and_columns(lines):
+    rows = []
+    for n in range(2, len(lines), 6):
+        # Rows.
+        board = lines[n:n + 5]
+        board = [[int(i) for i in row.split()] for row in board]
+        rows += board
+
+        # Add columns.
+        rows += [[row[i] for row in board] for i in range(len(board[0]))]
+    return rows
+
+
+def get_winning_row_index(drawn, rows):
+    count = [0 for _ in rows]
+    for d in drawn:
+        for i, row in enumerate(rows):
+            if d in row:
+                count[i] += 1
+                if count[i] == 5:
+                    return i // 10, d
+
+
+def get_score(board, drawn, last_drawn):
+    drawn_so_far = drawn[:drawn.index(last_drawn) + 1]
+    board = [[n for n in row if n not in drawn_so_far] for row in board]
+    return sum([sum(row) for row in board]) * last_drawn
+
+
+def calculate(lines):
+    drawn = [int(line) for line in lines[0].split(',')]
+    rows = get_rows_and_columns(lines)
+    row_index, last_drawn = get_winning_row_index(drawn, rows)
+    board = rows[row_index * 10:row_index * 10 + 5]
+    return get_score(board, drawn, last_drawn)
+
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+    print(calculate(lines))
